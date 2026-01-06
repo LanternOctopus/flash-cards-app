46a1ab931d19e52f32252b93552f55cb06b1ad54
Deepti Boddapati <145002352+LanternOctopus@users.noreply.github.com>
2026-01-07 03:24:46 +0530

    Created ReadOutLoud

---
+++ src/activities/ReadOutLoudView.tsx
@@ -0,0 +1,56 @@
+import { ReadOutLoudModel } from "./ReadOutLoudModel";
+import {
+    PageBuilderProvider,
+    usePageBuilder,
+} from "../components/PageBuilderCTX";
+import { ToggleBoxController } from "../components/ToggleBox";
+import { DataProvider } from "./DataProvider";
+import { useAnswer } from "./AnswerProvider";
+import { useQuestion } from "./QuestionContext";
+import { SequenceController } from "./SequenceController";
+export function ReadOutLoudScreen() {
+    return (
+        <DataProvider
+            questionPath="ReadOutLoud.yaml"
+            configPath="ReadOutLoudConfig.yaml"
+            storageKey="readOutLoud"
+        >
+            <PageBuilderProvider>
+                <ToggleBoxController />
+                <SequenceController
+                    modelClass={ReadOutLoudModel}
+                >
+                    <ReadOutLoud />
+                </SequenceController>
+            </PageBuilderProvider>
+        </DataProvider>
+    );
+}
+type ReadOutLoudItem = {
+    text: string;
+    translation?: string;
+    transliteration?: string;
+    answer: string;
+};
+export function ReadOutLoud() {
+    const builder = usePageBuilder();
+
+    const item = { ...useQuestion<ReadOutLoudItem>() };
+    const {
+        answer,
+        checkCorrectness,
+        onComplete,
+        handleNext,
+    } = useAnswer();
+    const slots = builder.slots;
+    slots.text = <div>{item.text}</div>;
+    slots.advance = (
+        <button onClick={handleNext}>Next</button>
+    );
+    const front = builder.buildFront();
+    return (
+        <>
+            <div>{front}</div>
+        </>
+    );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import "./index.css";
 // import App from "./App";
-import { PictureMatchingGameScreen } from "./activities/PictureMatchingGame";
+import { ReadOutLoudScreen } from "./activities/ReadOutLoudView";
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Root container not found");
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(container);
 root.render(
     <React.StrictMode>
         <HashRouter>
-            <PictureMatchingGameScreen />
+            <ReadOutLoudScreen />
         </HashRouter>
     </React.StrictMode>
 );
